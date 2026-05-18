b61dc659a3c7a312412b5e668bac5135921cd85f
iiswidyawati <fauzy1923@gmail.com>
2026-05-18 18:02:00 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+const text = "DEWI NUR OVILIA";
+
+let i = 0;
+
+function typing(){
+
+if(i < text.length){
+
+document.getElementById("typing").innerHTML += text.charAt(i);
+
+i++;
+
+setTimeout(typing,80);
+
+}
+
+}
+
+typing();
